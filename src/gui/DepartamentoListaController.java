09edfaa11a0bf4aa4d7d9858bae81afe781e53cc
Nadia Araujo <nadiaaraujo1@gmail.com>
2022-06-08 14:31:30 -0300

    Editando/Atualizando Departamento

--- src/gui/DepartamentoListaController.java
+++ src/gui/DepartamentoListaController.java
@@ -4,11 +4,11 @@ import java.io.IOException;
 import java.net.URL;
 import java.util.List;
 import java.util.ResourceBundle;
-
 import application.Main;
 import gui.listeners.DadoChangeListener;
 import gui.util.Alertas;
 import gui.util.Uteis;
+import javafx.beans.property.ReadOnlyObjectWrapper;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
@@ -18,6 +18,7 @@ import javafx.fxml.Initializable;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
+import javafx.scene.control.TableCell;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
@@ -27,7 +28,7 @@ import javafx.stage.Stage;
 import model.entities.Departamento;
 import model.services.DepartamentoService;
 
-public class DepartamentoListaController implements Initializable,DadoChangeListener {
+public class DepartamentoListaController implements Initializable, DadoChangeListener {
 
 	// Fazendo referencia/injeção de dependência à classe de serviço
 	private DepartamentoService depservice;// Não precisa forçar o new DepartamentoService, crio um método
@@ -41,15 +42,19 @@ public class DepartamentoListaController implements Initializable,DadoChangeList
 	private TableColumn<Departamento, String> tableColunaNome;// Referencia o Nome
 
 	@FXML
+	private TableColumn<Departamento, Departamento> tableColunaEDIT;
+
+	@FXML
 	private Button btNovo;
 
 	private ObservableList<Departamento> obsList;// Carrego os departamentos na obsList
 
 	// Tratamento de eventos do clique do botao
-	public void onBtNovoAction(ActionEvent evento) {//Referencia para o controle que recebeu o evento. E a partir desse evento, posso acessar o Stage
+	public void onBtNovoAction(ActionEvent evento) {// Referencia para o controle que recebeu o evento. E a partir desse
+													// evento, posso acessar o Stage
 		Stage parentStage = Uteis.currentStage(evento);
-		Departamento obj = new Departamento();//Iniciou vazio pois é o botao de novo
-		criaFormDialogo(obj,"/gui/DepartamentoForm.fxml", parentStage);//Aí é criada a janela de formulario
+		Departamento obj = new Departamento();// Iniciou vazio pois é o botao de novo
+		criaFormDialogo(obj, "/gui/DepartamentoForm.fxml", parentStage);// Aí é criada a janela de formulario
 	}
 
 	// Setando dependência (classe de serviço) pelo método
@@ -71,7 +76,7 @@ public class DepartamentoListaController implements Initializable,DadoChangeList
 
 		Stage stage = (Stage) Main.getMainScene().getWindow();// Referencia para a janela com o Stage em partentes pois
 																// o Main é superclasse
-
+		
 		// Deixar a parte debaixo na subjanela de Departamento acompanhando a parte
 		// inferior da tela
 		tableViewDepartamento.prefHeightProperty().bind(stage.heightProperty());
@@ -92,37 +97,63 @@ public class DepartamentoListaController implements Initializable,DadoChangeList
 		// Carregando a lista dentro do ObservableList
 		obsList = FXCollections.observableArrayList(lista);// Instancia o obsList da lista
 		tableViewDepartamento.setItems(obsList);// Mostrando, na tela, os dados instanciados na obsList
+	
+		iniciaEdicaoButtons();//Acrescenta um botao de edição em cada linha da tabela e vai abrir o formulario de edição usando o criaFormDialogo
 	}
 
-		private void criaFormDialogo(Departamento obj,String absolutoNome, Stage parentStage) {//Informando quem criou essa janela de dialogo
-			try {
-				FXMLLoader loader = new FXMLLoader(getClass().getResource(absolutoNome));
-				Pane painel = loader.load();
-				
-				//Referência para a tela de formulario no Controller
-				DepartamentoFormController dfc = loader.getController();
-				dfc.setDepartamento(obj);
-				dfc.setDepartamentoService(new DepartamentoService());//Injeção de depencia para funcionar o servico de salvar os dados no bd com o botao
-				dfc.subscribeDadoChangeListener(this);//executa o objeto do DepartamentoListController, dispara o evento e executa o metodo onDataChanged
-				dfc.atualizaFormDados();
-				
-				//Janela do formulario para preencher um novo Departamento
-				//Colocando outro Stage - Um palco na frente de outro
-				Stage dialogoStage = new Stage();
-				dialogoStage.setTitle("Entre com os dados de Departamento:");//Título para a janela
-				dialogoStage.setScene(new Scene(painel));//Novo Stage, nova cena
-				dialogoStage.setResizable(false);//Informa se a janela pode ou nao ser redimensionada (no caso, nao)
-				dialogoStage.initOwner(parentStage);//Entra o pai dessa janela 
-				dialogoStage.initModality(Modality.WINDOW_MODAL);//Se a janela será modal ou terá outro comportamento - Modal (tem que fechar esta para acessar a anterior)
-				dialogoStage.showAndWait();
-			}
-			catch (IOException e) {
-				Alertas.showAlert("IOException", "Erro ao carregar a view", e.getMessage(), AlertType.ERROR);
-			}
-		}
+	private void criaFormDialogo(Departamento obj, String absolutoNome, Stage parentStage) {// Informando quem criou
+																							// essa janela de dialogo
+		try {
+			FXMLLoader loader = new FXMLLoader(getClass().getResource(absolutoNome));
+			Pane painel = loader.load();
 
-		@Override
-		public void onDataChanged() {
-			atualizaTableView();//Chama o metodo que atualiza
+			// Referência para a tela de formulario no Controller
+			DepartamentoFormController dfc = loader.getController();
+			dfc.setDepartamento(obj);
+			dfc.setDepartamentoService(new DepartamentoService());// Injeção de depencia para funcionar o servico de
+																	// salvar os dados no bd com o botao
+			dfc.subscribeDadoChangeListener(this);// executa o objeto do DepartamentoListController, dispara o evento e
+													// executa o metodo onDataChanged
+			dfc.atualizaFormDados();
+
+			// Janela do formulario para preencher um novo Departamento
+			// Colocando outro Stage - Um palco na frente de outro
+			Stage dialogoStage = new Stage();
+			dialogoStage.setTitle("Entre com os dados de Departamento:");// Título para a janela
+			dialogoStage.setScene(new Scene(painel));// Novo Stage, nova cena
+			dialogoStage.setResizable(false);// Informa se a janela pode ou nao ser redimensionada (no caso, nao)
+			dialogoStage.initOwner(parentStage);// Entra o pai dessa janela
+			dialogoStage.initModality(Modality.WINDOW_MODAL);// Se a janela será modal ou terá outro comportamento -
+																// Modal (tem que fechar esta para acessar a anterior)
+			dialogoStage.showAndWait();
+		} catch (IOException e) {
+			Alertas.showAlert("IOException", "Erro ao carregar a view", e.getMessage(), AlertType.ERROR);
 		}
+	}
+
+	@Override
+	public void onDataChanged() {
+		atualizaTableView();// Chama o metodo que atualiza
+	}
+
+	private void iniciaEdicaoButtons() {
+		tableColunaEDIT.setCellValueFactory(param -> new ReadOnlyObjectWrapper<>(param.getValue()));
+		tableColunaEDIT.setCellFactory(param -> new TableCell<Departamento, Departamento>() {
+			
+			private final Button button = new Button("editar");
+
+			@Override
+			protected void updateItem(Departamento obj, boolean empty) {
+				super.updateItem(obj, empty);
+				if (obj == null) {
+					setGraphic(null);
+					return;
+				}
+				setGraphic(button);
+				button.setOnAction(
+						event -> criaFormDialogo (obj, "/gui/DepartamentoForm.fxml", Uteis.currentStage(event)));
+			}
+		});
+	}
+
 }
